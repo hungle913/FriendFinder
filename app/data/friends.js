8af5ac563265ee2ee33fd14cbc6248f46be174e7
Hung Le <hungle913@gmail.com>
2020-04-17 13:54:57 -0700

    added images and CSS file. Also started friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+	{
+		name: "Gizmo",
+		photo: "",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Guinness",
+		photo: "",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Kino",
+		photo: "",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "BG",
+		photo: "",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+];
+
+module.exports = friends;
