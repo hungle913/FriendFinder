5572483e0cadc9648272690fd17f2f4dd76d9953
Hung Le <hungle913@gmail.com>
2020-04-17 14:05:42 -0700

    replaced web links for new images in friends.js file.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
 	{
 		name: "Gizmo",
-        photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Gizmo.jpg?raw=true",
+        photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Gizmo.PNG?raw=true",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Guinness",
-		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Guiness.jpg?raw=true",
+		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Guiness.PNG?raw=true",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Kino",
-		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Kino.jpg?raw=true",
+		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Kino.PNG?raw=true",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "BG",
-		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/BG.jpg?raw=true",
+		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/BG.PNG?raw=true",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 ];
