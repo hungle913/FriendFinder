450478fc7ff136f102bf60844079b2419a35952a
Hung Le <hungle913@gmail.com>
2020-04-17 13:59:24 -0700

    Re-Size image files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
 	{
 		name: "Gizmo",
-		photo: "",
+        photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Gizmo.jpg?raw=true",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Guinness",
-		photo: "",
+		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Guiness.jpg?raw=true",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Kino",
-		photo: "",
+		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/Kino.jpg?raw=true",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "BG",
-		photo: "",
+		photo: "https://github.com/hungle913/FriendFinder/blob/master/app/public/Assets/images/BG.jpg?raw=true",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 ];
