54bdc8134aebb9aa4efee55f18d8d235add367f3
Hung Le <hungle913@gmail.com>
2020-04-22 10:35:02 -0700

    deleted some excess code and comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -43,16 +43,13 @@ module.exports = function(app) {
             }
         }
 
-        // IF 2 friends have same comparison, then newest entry is chosen
+        //Set best match
         bestPupMatch = friendsData[bestMatchPosition];
         // console.log("Best Pup is = " + bestPupMatch.name);
         // console.log("Best Pup is = " + bestPupMatch.photo);
 
-            // Replay with JSON object of best Pup match
-            // res.json(bestPupMatch)
-
-		// Send appropriate response
-        res.json(bestPupMatch);
+        // Replay with JSON object of best Pup match
+        res.json(bestPupMatch)
         
         // Add new user
 		friendsData.push(userInput);
